c410bca72c84c3d7b7dd71e4655ce96d6c1a1a52
J. Singleton <jennifer.singleton@gmail.com>
2019-04-15 14:27:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     set :sessions, true
-    set :session_secret, 
+    set :session_secret, SecureRandom.hex(64)
   end
 end
