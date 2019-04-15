ad55c910aff91f5057b528ea753e926bad067ab3
J. Singleton <jennifer.singleton@gmail.com>
2019-04-15 14:39:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-    rack.session[:item] = params[:item]
+    session[:item] = params[:item]
     @session[:item] = session
 
     erb :checkout
